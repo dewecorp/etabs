c787465d8e79f3d61fd4c07b17ea1983667e5789
dewecorp <ibnuhasan3@gmail.com>
2026-03-04 21:36:30 +0700

    update baru

--- plugins/proses-ajax.php
+++ plugins/proses-ajax.php
@@ -4,7 +4,8 @@ include "../inc/rupiah.php";
 
  $output = '';  
  if(isset($_POST["nis"])) {  
-        $sql = "select sum(setor)-sum(tarik) as Total from tb_tabungan where nis= '".$_POST["nis"]."'";
+        $nis = mysqli_real_escape_string($koneksi, $_POST["nis"]);
+        $sql = "select sum(setor)-sum(tarik) as Total from tb_tabungan where nis= '$nis'";
         $hasil = mysqli_query($koneksi, $sql);  
         while($row = mysqli_fetch_array($hasil)) {  
             $output = $row["Total"];
